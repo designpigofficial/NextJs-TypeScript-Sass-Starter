1b2c70e8f3e0664e508e9304ef998c287453457c
Rakib Ahmed <rakib987@gmail.com>
2022-04-02 13:00:43 -0400

    props

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,7 +1,12 @@
-import '../globals.scss'
-
-function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+import '../globals.scss';
+import React, { FC } from "react";
+declare global { 
+  interface Component {
+    [key: string]: any
+  }
+  interface pageProps {
+    [key: string]: any
+  }
 }
-
+const MyApp: FC<Component> = ({ Component, pageProps }) => <Component {...pageProps} />;
 export default MyApp
